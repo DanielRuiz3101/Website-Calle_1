721dde8ff1e45900b9667b7ffa5a30566bbaa507
DanielRuiz3101 <darp0131@gmail.com>
2026-08-03 15:06:53 -0400

    Initial Setup with vue and tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Inicio</RouterLink> |
+      <RouterLink to="/about">Nosotros</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
